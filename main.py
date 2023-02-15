bfd48750da2b07c448842c5e5e376fc5cd2da391
HyperPyCoder <80395140+Abhijith-Sudhakaran@users.noreply.github.com>
2023-02-15 21:50:51 +0530

    Create main.py
    Created, Detailed for first time 😂

---
+++ main.py
@@ -0,0 +1,44 @@
+'''
+
+# Fake Crash Python file for Pranking 😂
+# Author GitHub: @Abhijith-Sudhakaran
+# Instagram - @hypercat_ext
+
+'''
+
+import colorama
+import datetime 
+from colorama import Fore
+from datetime import datetime
+
+current_dateTime = datetime.now()
+x = 8
+y = 5
+
+while y <= x:
+  print(Fore.RED, current_dateTime.hour,':', current_dateTime.minute,':', current_dateTime.second, "|", "Address : System Crashed,✓ internal error caused by undefined variables while lopping. Printing...")
+
+'''
+
+# If you want print these function in one line use `break` function as mentioned below.
+# if you want to change colour of the line. 
+# • change - Fore.<colour>
+# • Eg- print(Fore.RED, <text>) - Red Colour 
+
+# -> Available Colours are, Search Colorama on Google for Detailed info.
+
+• RED
+• GREEN
+• BLUE
+• BLACK
+• WHITE
+• YELLOW
+• CYAN
+-
+- Example to stop above function continues printing,
+
+while y <= x:
+  print(Fore.<colour>, <text you want>)
+  break
+
+'''
